8835de80a19d486ced009797537fafe532ffc529
Surya_K_Ratheesh <143280157+Surya-K-Ratheesh@users.noreply.github.com>
2025-06-24 17:32:39 +0530

    Delete ERAM_TECH directory

--- ERAM_TECH/src/utils/aiHelper.js
+++
@@ -1,8 +0,0 @@
-export async function loadModel() {
-  const model = await tf.loadGraphModel('path/to/model.json')
-  return model
-}
-
-export async function autoCrop(image) {
-  // Implementation for smart cropping
-}
